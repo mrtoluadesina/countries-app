9bafdf90f809a5bdcd368e01cfb619a4a682e11b
Tolu Adesina <tadesina90@gmail.com>
2019-09-05 22:47:00 +0100

    Added Header Component to the App.js Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from './components/Header';
 import Home from './container/Home';
 import "./App.scss";
 
 function App() {
   return (
     <>
+    <Header />
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
