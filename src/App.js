cd155c3d7029918ae4e2ce047a3de2d7273bdc4d
Tolu Adesina <tadesina90@gmail.com>
2019-09-06 19:14:11 +0100

    Abstracted the routes into AppRoutes Component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import Header from "./components/Header";
-import Home from "./container/Home";
-import Countries from "./container/Countries";
-import Continents from "./container/Continents";
-
-import styled from "styled-components";
+import AppRoutes from "./router";
 import "./App.scss";
 
 function App() {
-  return (
-    <Wrapper>
-      <Header />
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/countries" exact component={Countries} />
-          <Route path="/continents" exact component={Continents} />
-        </Switch>
-      </Router>
-    </Wrapper>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
-
-const Wrapper = styled.div`
-  height: 100vh;
-`;
