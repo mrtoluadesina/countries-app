a993d24e49384cbc810c03a4ad479f4aea7432c9
Tolu Adesina <tadesina90@gmail.com>
2019-09-06 12:28:25 +0100

    Added some new routes to the App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from './components/Header';
-import Home from './container/Home';
+import Header from "./components/Header";
+import Home from "./container/Home";
+import Countries from "./container/Countries";
+import Continents from "./container/Continents";
+
+import styled from "styled-components";
 import "./App.scss";
 
 function App() {
   return (
-    <>
-    <Header />
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
-    </Router>
-    </>
+    <Wrapper>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/countries" exact component={Countries} />
+          <Route path="/continents" exact component={Continents} />
+        </Switch>
+      </Router>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  height: 100vh;
+`;
