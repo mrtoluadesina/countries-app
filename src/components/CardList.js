540bf532449663f681d3293ea7a525160cd27cd7
Tolu Adesina <tadesina90@gmail.com>
2019-09-07 08:08:16 +0100

    passed props properly to handle both continent and contries

---
+++ src/components/CardList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { SimpleCard } from "./Cards";
+
+export default function CardList({ data, className }) {
+  return (
+    <Container className={className}>
+      {data.map((data, index) => (
+        <Link
+          to={{ pathname: `/countries/${data.code}`, state: data }}
+          style={{ color: "#51525c" }}
+          key={index}
+        >
+          <SimpleCard data={data} />
+        </Link>
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled.section`
+  margin-top: 3rem;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  flex-wrap: wrap;
+
+  @media (min-width: 420px) {
+    justify-content: space-between;
+    &::after {
+      content: '';
+      flex: auto;
+    }
+  }
+`;
