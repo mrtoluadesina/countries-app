53f26cd1bedc094429cc8c844854e771d3db2dee
Tolu Adesina <tadesina90@gmail.com>
2019-09-10 07:12:27 +0100

    Added single country route to the router file

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "../views/Home";
 import Countries from "../views/Countries";
+import Country from "../views/Country";
 import Continents from "../views/Continents";
 
 const AppRoutes = () => {
@@ -9,6 +10,7 @@ const AppRoutes = () => {
     <Router>
       <Route path="/" exact component={Home} />
       <Route path="/countries" exact component={Countries} />
+      <Route path="/countries/:code" exact component={Country} />
       <Route path="/continents" exact component={Continents} />
     </Router>
   );
