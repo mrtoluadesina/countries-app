aa4bba75b95f4c517909a21e4cc91920468ee6ae
Tolu Adesina <tadesina90@gmail.com>
2019-09-14 03:34:59 +0100

    updated cards list grid template columns

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -28,7 +28,7 @@ const Container = styled.section`
 
   @media (min-width: 420px) {
     display: grid;
-    grid-template-columns: repeat(6, 1fr);
+    grid-template-columns: repeat(5, 1fr);
     grid-gap: 10px;
     &::after {
       content: '';
