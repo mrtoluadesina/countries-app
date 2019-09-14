1cc691846819baca4bb036b92d3cbb5e890c295b
Tolu Adesina <tadesina90@gmail.com>
2019-09-14 04:01:22 +0100

    Added a new route for the countries under a continent

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ const AppRoutes = () => {
       <Route path="/countries/:code" exact component={Country} />
       <Route path="/continents" exact component={Continents} />
       <Route path="/continents/:code" exact component={Continent} />
+      <Route path="/continents/countries/:code" exact component={Country} />
     </Router>
   );
 };
